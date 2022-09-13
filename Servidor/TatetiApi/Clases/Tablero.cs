9ac09a2eb46ea5faabcbcf1488d45aceb5b25197
GPR <rochafrancoagustin@hotmail.com>
2022-09-13 20:37:38 -0300

    Se asignaron las filas al tablero

--- Servidor/TatetiApi/Clases/Tablero.cs
+++ Servidor/TatetiApi/Clases/Tablero.cs
@@ -2,6 +2,9 @@
 {
     public class Tablero
     {
+        public int[] filaUno { get; set; } = new int[3];
+        public int[] filaDos { get; set; } = new int[3];
+        public int[] filaTres { get; set; } = new int[3];
 
     }
 }
