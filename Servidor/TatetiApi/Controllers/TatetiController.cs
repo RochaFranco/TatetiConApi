42d25575ac413f3dc0d36c19bf3b9394f198e914
GPR <rochafrancoagustin@hotmail.com>
2022-09-13 20:14:07 -0300

    Se creo el proyecto

---
+++ Servidor/TatetiApi/Controllers/TatetiController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TatetiApi.Controllers
+{
+    [ApiController]
+    public class TatetiController : Controller
+    {
+        [HttpPost]
+        [Route("crearTablero")]
+        public void crearTablero()
+        {
+
+        }
+
+        [HttpGet]
+        [Route("verTablero")]
+        public void verTablero()
+        {
+
+        }
+
+        [HttpPut]
+        [Route("ponerFicha")]
+        public void ponerFicha()
+        {
+
+        }
+    }
+}
